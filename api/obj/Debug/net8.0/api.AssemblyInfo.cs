28bee375774955dde34d8ba75253d391f79098df
q1bczyk <bartekkubik7@gmail.com>
2024-01-23 22:09:20 +0100

    ignore

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187dd5fe88e2f03504a2e39dc005e344f4cb44e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e90841c9c5294b508f13961cf38c79f3c50a3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
