304dcb3fa1b56b3aa9ad69769cf156040cf6810d
q1bczyk <bartekkubik7@gmail.com>
2024-02-10 14:27:38 +0100

    dodanie do api metody pobierajacej lokale

--- api/_Interfaces/ILocalRepository.cs
+++ api/_Interfaces/ILocalRepository.cs
@@ -1,6 +1,7 @@
 public interface ILocalRepository
 {
     void Update(Local local);
+    Task<List<Local>> GetLocalsAsync();
     Task<Local> AddLocalAsync(Local local);
     Task<bool> IsLocalExists(string id);
     Task<bool> SaveAllAsync();
